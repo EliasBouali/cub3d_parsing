00d5dc1289d173413ac6f931fdfc3d3225f610a1
Bouali <elias.bouali90@gmail.com>
2025-11-20 22:47:20 +0100

    parsing ameliorer

--- src/parse/parse_check_map_closed.c
+++ src/parse/parse_check_map_closed.c
@@ -1,18 +1,5 @@
 #include "cubd3.h"
 
-
-static int is_on_border(int row, int col, int rows, int cols)
-{
-  if (row == 0)
-    return (1);
-  if (row == rows - 1)
-    return (1);
-  if (col == 0)
-    return (1);
-  if (col == cols - 1)
-    return (1);
-  return (0);
-}
 static int neighbor_is_space(char **lines_map, int row, int col)
 {
   if (lines_map[row - 1][col] == ' ')
@@ -43,8 +30,6 @@ int check_map_closed(t_map *map)
       c = map->lines_map[row][col];
       if (c == '0')
       {
-        if (is_on_border(row, col, map->rows, map->cols))
-          return (0);
         if (neighbor_is_space(map->lines_map, row, col))
           return (0);
       }
